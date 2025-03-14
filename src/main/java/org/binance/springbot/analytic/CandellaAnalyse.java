805ad18c399219e50f98c628e7268682a3d09abf
radomyr <4rad@i.ua>
2025-03-14 23:21:20 +0100

    First Beta Version SpringBot ver. 1.00

--- src/main/java/org/binance/springbot/analytic/CandellaAnalyse.java
+++ src/main/java/org/binance/springbot/analytic/CandellaAnalyse.java
@@ -1,34 +1,53 @@
 package org.binance.springbot.analytic;
 
+import com.binance.api.client.domain.market.Candlestick;
+import org.ta4j.core.Bar;
 import org.ta4j.core.BarSeries;
+
 
 public class CandellaAnalyse {
 
     private int trend;
+    private double bullSpred = 0;
+    private double bullVolume = 0;
+    private double bearSpred = 0;
+    private double bearVolume =0;
+    private static int bullIndex = 0;
+    private static int bearIndex = 0;
 
     public CandellaAnalyse() {
     }
 
     public static double trendDetect(BarSeries series, String LowLimit, String HighLimit) {
-        int i =0;
-        int trend = 0;
+        int i = series.getEndIndex()-1;
+
         double volumeMax = 0;
         while (true) {
-            if (series.getBar(series.getEndIndex()-i).getHighPrice().doubleValue() == Double.valueOf(HighLimit)
-                && series.getBar(series.getEndIndex()-i).getLowPrice().doubleValue() == Double.valueOf(LowLimit)
+            if (series.getBar(i).getHighPrice().doubleValue() == Double.valueOf(HighLimit)
+                && series.getBar(i).getLowPrice().doubleValue() == Double.valueOf(LowLimit)
             ){
+                break;
 
-                return volumeMax;
             }
-            if (Math.abs(volumeMax) < series.getBar(series.getEndIndex()-i).getVolume().doubleValue()) {
-                volumeMax = series.getBar(series.getEndIndex()-i).getVolume().doubleValue();
-                if (series.getBar(series.getEndIndex()-i).getOpenPrice().doubleValue() > series.getBar(series.getEndIndex()-i).getClosePrice().doubleValue()){
-                    volumeMax =  - volumeMax;
+            if (series.getBar(i).getOpenPrice().doubleValue() < series.getBar(i).getClosePrice().doubleValue())   //Long
+            { if (bullIndex == 0 || getSpread(series.getBar(i)) > getSpread(series.getBar(bullIndex)))
+                { bullIndex = i;}
                 }
-            }
-         i++;
-        }
+            if (series.getBar(i).getOpenPrice().doubleValue() > series.getBar(i).getClosePrice().doubleValue())   // Short
+            {   if (bearIndex == 0 || getSpread(series.getBar(i)) > getSpread(series.getBar(bearIndex)))
+                { bearIndex = i;}}
+
+         i--;
 
     }
+        if (series.getBar(bearIndex).getVolume().doubleValue() > series.getBar(bullIndex).getVolume().doubleValue()) {return -1;}
+        if (series.getBar(bearIndex).getVolume().doubleValue() < series.getBar(bullIndex).getVolume().doubleValue()) {return 1;}
+        return 0;
+    }
+        private static double getSpread(Bar bar) {
+            return  Math.abs(bar.getOpenPrice().doubleValue()-bar.getClosePrice().doubleValue());
+        }
 
 }
+
+
